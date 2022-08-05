c06d8eba383c9a8211d55803d9482a6eb594fd1b
Madhur Tandon <madhurtandon23@gmail.com>
2022-08-05 20:19:43 +0530

    wip kubernetes support

--- mlem/contrib/docker/base.py
+++ mlem/contrib/docker/base.py
@@ -330,7 +330,9 @@ class DockerEnv(MlemEnv[DockerContainer]):
     type: ClassVar = "docker"
     deploy_type: ClassVar = DockerContainer
     registry: DockerRegistry = DockerRegistry()
-    daemon: DockerDaemon = DockerDaemon(host="")
+    daemon: DockerDaemon = DockerDaemon(
+        host=os.getenv("DOCKER_HOST", default="")
+    )
 
     def delete_image(self, image: DockerImage, force: bool = False, **kwargs):
         with self.daemon.client() as client:
